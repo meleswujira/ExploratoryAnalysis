5fd7fdce2f0898b20cce39746baf7e3887f51f2b
meleswujira <manzeno@gmail.com>
2014-07-24 00:38:44 -0700

    Across US emission from coal b/n 1999 and 2008

---
+++ Plot4.R
@@ -0,0 +1,19 @@
+CoalCombustionSources <- SCC[SCC$EI.Sector == "Fuel Comb - Comm/Institutional - Coal", 
+                             ]["SCC"]
+
+## Subset emissions due to coal combustion sources from 'NEI'
+emissionFromCoal <- NEI[NEI$SCC %in% CoalCombustionSources$SCC, ]
+## Calculate the emissions due to coal each year across United States
+totalCoalEmissionsByYear <- tapply(emissionFromCoal$Emissions, emissionFromCoal$year, sum)
+## Create the plot
+png(filename="figure/plot4.png", width=480, height=480)
+plot(totalCoalEmissionsByYear, x = rownames(totalCoalEmissionsByYear), type = "n", 
+     axes = FALSE, ylab = expression("Coal Related PM"[2.5] * " Emission (in tons)"), 
+     xlab = "Year", main = expression("Coal Related PM"[2.5] * " Emission across United States (1999 - 2008)"))
+points(totalCoalEmissionsByYear, x = rownames(totalCoalEmissionsByYear), pch = 16, 
+       col = "red")
+lines(totalCoalEmissionsByYear, x = rownames(totalCoalEmissionsByYear), col = "blue")
+axis(2)
+axis(side = 1, at = seq(1999, 2008, by = 3))
+box()
+dev.off()
